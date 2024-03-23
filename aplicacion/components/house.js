3ec18d86d7e4233bd1cbddf7b5230f8f92118d44
Javier Criado <javiercriado@users.noreply.github.com>
2024-03-23 17:58:51 +0000

    actualizaciones finales del curso

--- aplicacion/components/house.js
+++ aplicacion/components/house.js
@@ -2,10 +2,19 @@ import { useContext } from "react";
 import currencyFormatter from "../helpers/currencyFormatter";
 import defaultPhoto from "../helpers/defaultPhoto";
 import { navigationContext } from "./app";
-import Bids from "./bids";
+import BidList from "./bidList";
+import useBids from "../hooks/useBids";
+import AddBid from "./addBid";
+import LoadingIndicator from "./loadingIndicator";
+import loadingStatus from "@/helpers/loadingStatus";
 
 const House = () => {
   const { param: house } = useContext(navigationContext);
+  const {bids, loadingState, addBid } = useBids(house.id);
+
+  if (loadingState !== loadingStatus.loaded)
+    return <LoadingIndicator loadingState={loadingState} />;
+
   return (
       <div className="row">
           <div className="col-6">
@@ -34,7 +43,8 @@ const House = () => {
       <div className="row">
         <div className="col-12 mt-3">{house.description}</div>
       </div>
-      <Bids house={house} />
+      <BidList bids={bids} />
+      <AddBid house={house} addBid={addBid} />
     </div>
   </div>
   );  
